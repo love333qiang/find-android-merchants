c40238d422c37dfcc50085a2ff4fa52145bc53ee
Administrator <Administrator@RQYRFSOZQY89QAA>
2014-01-22 16:28:14 +0800

    删除Menu

--- src/com/runyetech/find2/merchants/RegisterActivity.java
+++ src/com/runyetech/find2/merchants/RegisterActivity.java
@@ -1,10 +1,9 @@
 package com.runyetech.find2.merchants;
 
-import com.runyetech.find2_android_merchants.R;
-
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+
+import com.runyetech.find2_android_merchants.R;
 
 public class RegisterActivity extends Activity {
 
@@ -13,12 +12,4 @@ public class RegisterActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.register, menu);
-		return true;
-	}
-
 }

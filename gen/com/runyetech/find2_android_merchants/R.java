4518915470b046e4e84a4480293173e7b68744bb
Administrator <Administrator@RQYRFSOZQY89QAA>
2014-01-24 19:04:44 +0800

    选择商品分类

--- gen/com/runyetech/find2_android_merchants/R.java
+++ gen/com/runyetech/find2_android_merchants/R.java
@@ -8,6 +8,11 @@
 package com.runyetech.find2_android_merchants;
 
 public final class R {
+    public static final class array {
+        /**  商品分类 
+         */
+        public static final int goods_category=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -34,80 +39,87 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int RadioButton01=0x7f080021;
-        public static final int RadioButton02=0x7f080020;
-        public static final int RadioGroup01=0x7f08001f;
-        public static final int TextView01=0x7f08001e;
-        public static final int TextView02=0x7f080037;
-        public static final int TextView03=0x7f080039;
-        public static final int TextView04=0x7f08003b;
-        public static final int TextView05=0x7f08003d;
-        public static final int TextView06=0x7f08003f;
-        public static final int action_settings=0x7f080049;
-        public static final int activity_login_button_Login=0x7f08002e;
-        public static final int activity_login_button_Register=0x7f08002f;
-        public static final int activity_login_editText_UserName=0x7f08002c;
-        public static final int activity_login_editText_UserPswd=0x7f08002d;
-        public static final int addGoods=0x7f080030;
-        public static final int addGoods_ChoiseWords=0x7f080029;
-        public static final int addGoods_PublisherGoods=0x7f08002b;
-        public static final int addGoods_SearchGo=0x7f080027;
-        public static final int addGoods_SearchWorlds=0x7f080026;
-        public static final int button1=0x7f080018;
-        public static final int editText1=0x7f080006;
-        public static final int editText2=0x7f080008;
-        public static final int editText3=0x7f08000a;
-        public static final int editText4=0x7f08000c;
-        public static final int editText5=0x7f08000f;
-        public static final int editText6=0x7f080012;
-        public static final int editText7=0x7f080015;
-        public static final int imageView1=0x7f080019;
-        public static final int imageView2=0x7f08001a;
-        public static final int imageView3=0x7f08001b;
-        public static final int layout1=0x7f08001c;
-        public static final int layout2=0x7f08001d;
-        public static final int layout_Register_First=0x7f080031;
-        public static final int layout_Register_sceond=0x7f080032;
-        public static final int linearLayout1=0x7f080025;
-        public static final int linearlayout2=0x7f080028;
-        public static final int linearlayout3=0x7f08002a;
-        public static final int merchants_Register_BusinessDeadline=0x7f080044;
-        public static final int merchants_Register_ChooiseLicensePic=0x7f080046;
-        public static final int merchants_Register_ChooiseUserAvatar=0x7f080033;
-        public static final int merchants_Register_ComPanyName=0x7f080041;
-        public static final int merchants_Register_Email=0x7f080036;
-        public static final int merchants_Register_FirmName=0x7f080042;
-        public static final int merchants_Register_LicensePic=0x7f080047;
-        public static final int merchants_Register_License_Address=0x7f080043;
-        public static final int merchants_Register_MarketAddress=0x7f080045;
-        public static final int merchants_Register_MarketingCode=0x7f080040;
-        public static final int merchants_Register_NickName=0x7f080035;
-        public static final int merchants_Register_Phone_First=0x7f080038;
-        public static final int merchants_Register_Phone_Sceond=0x7f08003a;
-        public static final int merchants_Register_Pswd=0x7f08003c;
-        public static final int merchants_Register_UserAvater=0x7f080034;
-        public static final int merchants_Register_VerifyPswd=0x7f08003e;
-        public static final int radio0=0x7f080023;
-        public static final int radio1=0x7f080024;
-        public static final int radioGroup1=0x7f080022;
-        public static final int register_Submin=0x7f080048;
-        public static final int scrollView1=0x7f080000;
-        public static final int textView1=0x7f080001;
-        public static final int textView10=0x7f08000e;
-        public static final int textView11=0x7f080010;
-        public static final int textView12=0x7f080011;
-        public static final int textView13=0x7f080013;
-        public static final int textView14=0x7f080014;
-        public static final int textView15=0x7f080016;
-        public static final int textView16=0x7f080017;
-        public static final int textView2=0x7f080002;
-        public static final int textView3=0x7f080003;
-        public static final int textView4=0x7f080004;
-        public static final int textView5=0x7f080005;
-        public static final int textView6=0x7f080007;
-        public static final int textView7=0x7f080009;
-        public static final int textView8=0x7f08000b;
-        public static final int textView9=0x7f08000d;
+        public static final int TextView01=0x7f09001f;
+        public static final int TextView02=0x7f09003d;
+        public static final int TextView03=0x7f09003f;
+        public static final int TextView04=0x7f090041;
+        public static final int TextView05=0x7f090043;
+        public static final int TextView06=0x7f090045;
+        public static final int action_settings=0x7f090050;
+        public static final int activity_login_button_Login=0x7f090034;
+        public static final int activity_login_button_Register=0x7f090035;
+        public static final int activity_login_editText_UserName=0x7f090032;
+        public static final int activity_login_editText_UserPswd=0x7f090033;
+        public static final int addGoods=0x7f090036;
+        public static final int addGoods_ChoiseWords=0x7f09002f;
+        public static final int addGoods_PublisherGoods=0x7f090031;
+        public static final int addGoods_SearchGo=0x7f09002d;
+        public static final int addGoods_SearchWorlds=0x7f09002c;
+        public static final int goods_Brand=0x7f090004;
+        public static final int goods_ChoiseGoodsPic=0x7f090018;
+        public static final int goods_Counts=0x7f090015;
+        public static final int goods_Direction=0x7f09001d;
+        public static final int goods_FixedPrice=0x7f09000c;
+        public static final int goods_HasInvoice=0x7f090021;
+        public static final int goods_HasRepair=0x7f090025;
+        public static final int goods_Invoice=0x7f090020;
+        public static final int goods_Name=0x7f09000a;
+        public static final int goods_NetWeight=0x7f090012;
+        public static final int goods_NoInvoice=0x7f090022;
+        public static final int goods_NoRepair=0x7f090026;
+        public static final int goods_OriginalPrice=0x7f09000f;
+        public static final int goods_ParkType=0x7f090006;
+        public static final int goods_ProduceData=0x7f090008;
+        public static final int goods_Promise=0x7f090028;
+        public static final int goods_PublisherGoods=0x7f09002a;
+        public static final int goods_Repair=0x7f090024;
+        public static final int goods_category=0x7f09004f;
+        public static final int goods_pic_1=0x7f090019;
+        public static final int goods_pic_2=0x7f09001a;
+        public static final int goods_pic_3=0x7f09001b;
+        public static final int layout1=0x7f09001c;
+        public static final int layout2=0x7f09001e;
+        public static final int layout3=0x7f090027;
+        public static final int layout4=0x7f090029;
+        public static final int layout_Register_First=0x7f090037;
+        public static final int layout_Register_sceond=0x7f090038;
+        public static final int linearLayout1=0x7f09002b;
+        public static final int linearlayout2=0x7f09002e;
+        public static final int linearlayout3=0x7f090030;
+        public static final int merchants_Register_BusinessDeadline=0x7f09004a;
+        public static final int merchants_Register_ChooiseLicensePic=0x7f09004c;
+        public static final int merchants_Register_ChooiseUserAvatar=0x7f090039;
+        public static final int merchants_Register_ComPanyName=0x7f090047;
+        public static final int merchants_Register_Email=0x7f09003c;
+        public static final int merchants_Register_FirmName=0x7f090048;
+        public static final int merchants_Register_LicensePic=0x7f09004d;
+        public static final int merchants_Register_License_Address=0x7f090049;
+        public static final int merchants_Register_MarketAddress=0x7f09004b;
+        public static final int merchants_Register_MarketingCode=0x7f090046;
+        public static final int merchants_Register_NickName=0x7f09003b;
+        public static final int merchants_Register_Phone_First=0x7f09003e;
+        public static final int merchants_Register_Phone_Sceond=0x7f090040;
+        public static final int merchants_Register_Pswd=0x7f090042;
+        public static final int merchants_Register_UserAvater=0x7f09003a;
+        public static final int merchants_Register_VerifyPswd=0x7f090044;
+        public static final int register_Submin=0x7f09004e;
+        public static final int scrollView1=0x7f090000;
+        public static final int textView1=0x7f090001;
+        public static final int textView10=0x7f09000e;
+        public static final int textView11=0x7f090010;
+        public static final int textView12=0x7f090011;
+        public static final int textView13=0x7f090013;
+        public static final int textView14=0x7f090014;
+        public static final int textView15=0x7f090016;
+        public static final int textView16=0x7f090017;
+        public static final int textView2=0x7f090002;
+        public static final int textView3=0x7f090003;
+        public static final int textView4=0x7f090023;
+        public static final int textView5=0x7f090005;
+        public static final int textView6=0x7f090007;
+        public static final int textView7=0x7f090009;
+        public static final int textView8=0x7f09000b;
+        public static final int textView9=0x7f09000d;
     }
     public static final class layout {
         public static final int activity_goodsadd=0x7f030000;
@@ -118,11 +130,12 @@ public final class R {
         public static final int activity_register=0x7f030005;
         public static final int include_register_first=0x7f030006;
         public static final int include_register_second=0x7f030007;
+        public static final int popup_goodscategory=0x7f030008;
     }
     public static final class menu {
-        public static final int login=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int register=0x7f070002;
+        public static final int login=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int register=0x7f080002;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
@@ -196,10 +209,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/runyetech/find2/merchants/GoodsCategoryActivity.java
+++ src/com/runyetech/find2/merchants/GoodsCategoryActivity.java
@@ -1,13 +1,23 @@
 package com.runyetech.find2.merchants;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 
+import com.runyetech.find2.merchants.util.LogInfoPrint;
 import com.runyetech.find2_android_merchants.R;
 
 /**
@@ -24,7 +34,10 @@ public class GoodsCategoryActivity extends Activity {
 	private Button button_SearchGo;
 	/** 发布商品的按钮 */
 	private Button button_PublisherGoods;
-
+	/** 选择商品分类的Pop */
+	private PopupWindow window;
+	/** 商品分类的数组 */
+	private String[] item;
 	/** 按键事件监听类 */
 	private ClickListener listener;
 
@@ -36,11 +49,17 @@ public class GoodsCategoryActivity extends Activity {
 		initUI();
 	}
 
+	/**
+	 * 初始化UI布局控件
+	 */
 	private void initUI() {
+
+		item = getResources().getStringArray(R.array.goods_category);
 
 		listener = new ClickListener();
 
 		editText_ChoiseWords = (EditText) findViewById(R.id.addGoods_ChoiseWords);
+		editText_ChoiseWords.setOnClickListener(listener);
 		editText_SearchWords = (EditText) findViewById(R.id.addGoods_SearchWorlds);
 		button_PublisherGoods = (Button) findViewById(R.id.addGoods_PublisherGoods);
 		button_PublisherGoods.setOnClickListener(listener);
@@ -48,6 +67,10 @@ public class GoodsCategoryActivity extends Activity {
 		button_SearchGo.setOnClickListener(listener);
 	}
 
+	/**
+	 * @author YangFan 按键监听类
+	 * 
+	 */
 	class ClickListener implements OnClickListener {
 		public void onClick(View v) {
 			switch (v.getId()) {
@@ -57,7 +80,69 @@ public class GoodsCategoryActivity extends Activity {
 				break;
 			case R.id.addGoods_SearchGo:
 				break;
+			case R.id.addGoods_ChoiseWords:
+				createPopup(v);
+				break;
 			}
 		}
+	}
+
+	/**
+	 * 根据父控件的位置创建一个PopupWindow
+	 * 
+	 * @param parent
+	 *            父控件
+	 */
+	@SuppressWarnings("deprecation")
+	private void createPopup(View parent) {
+		if (window == null) {
+			LayoutInflater lay = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View v = lay.inflate(R.layout.popup_goodscategory, null);
+			v.setBackgroundColor(Color.BLUE);
+
+			ListView listView_Catgegory = (ListView) v.findViewById(R.id.goods_category);
+			listView_Catgegory.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, item));
+			listView_Catgegory.setOnItemClickListener(new ItemClickListener());
+
+			window = new PopupWindow(v, 200, 350);
+			window.setFocusable(true);
+			window.setBackgroundDrawable(new BitmapDrawable());
+			window.setOutsideTouchable(true);
+			window.update();
+		}
+		showPop(parent);
+	}
+
+	/**
+	 * 显示或者隐藏PopUpWindow
+	 * 
+	 * 如果显示就隐藏了，如果隐藏就根据父控件的位置去显示
+	 * 
+	 * @param parent
+	 *            父控件
+	 */
+	private void showPop(View parent) {
+		if (window.isShowing()) {
+			window.dismiss();
+		} else {
+			window.showAsDropDown(parent);
+		}
+	}
+
+	/**
+	 * @author YangFan ListView的Item的点击事件监听
+	 */
+	class ItemClickListener implements OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> arg0, View view, int position, long id) {
+			editText_ChoiseWords.setText(item[position]);
+			LogInfoPrint.i(true, "您当前选择的是：" + item[position]);
+		}
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		window = null;// 退出是让PopUp为NULL；
 	}
 }
